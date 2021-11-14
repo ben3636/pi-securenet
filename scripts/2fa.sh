3af2a994bf6542b04019a528c6e292421f41a6f3
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-11-14 15:12:13 -0500

    Update 2fa.sh

--- scripts/2fa.sh
+++ scripts/2fa.sh
@@ -2,5 +2,5 @@
 echo "New Key Created!"
 key=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 5)
 message="Pi IDS 2FA Key Is: "
-curl -X POST -H "Content-Type: application/json" -d '{"value1":"'"$message"'","value2":"'"$key"'","value3":"'""'"}' https://maker.ifttt.com/trigger/Pi-IDS/with/key/UntimRA5FlDbyVp5GRuNU >> /dev/null
+curl -X POST -H "Content-Type: application/json" -d '{"value1":"'"$message"'","value2":"'"$key"'","value3":"'""'"}' https://maker.ifttt.com/trigger/XXXXXXXXX >> /dev/null
 htpasswd -b /home/ben/.htpasswd ben $key
