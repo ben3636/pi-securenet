af276e849d640636eb225d81c31a1f3487bf07e6
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-11-21 15:55:39 -0500

    Added ability to display outbound UFW rules

--- scripts/ufw.sh
+++ scripts/ufw.sh
@@ -21,27 +21,48 @@ function table(){
 cat UFW.html.p1
 
 ### Parse the list of rules and extract fields
-ufw status | grep -v '(v6)' | sed '1,4d' | sed '/^$/d' \
+ufw status | grep -v '(v6)' | sed '1,4d' | sed '/^$/d' | sort -k4 \
 | while read line
 do
-	# Determine if rule is by port or service and parse accordingly
-	if [[ $line != *"tcp"* && $line != *"udp"* ]]
+	# Determine if rule is inbound or outbound and parse accordingly
+	f1=$(echo $line | awk ' { print $3 } ')
+        f2=$(echo $line | awk ' { print $4 } ')
+	if [[ $f1 == "OUT" ]] || [[ $f2 == "OUT" ]]
 	then
-		port=$(echo $line | awk ' { print $1 } ')
-                action=$(echo $line | awk ' { print $2 } ')
-                proto='N/A'
-                source=$(echo $line | awk ' { print $3 } ')
-
-		# Pass fields to table function for HTML formatting
-                table $port $proto $action $source
-	else
-		port=$(echo $line | awk ' { print $1 } '| awk -F '/' ' { print $1 } ')
-        	proto=$(echo $line | awk ' { print $1 } '| awk -F '/' ' { print $2 } ')
-		action=$(echo $line | awk ' { print $2 } ')
-        	source=$(echo $line | awk ' { print $3 } ')
+		port_and_dest=$(echo $line | grep OUT | grep -oe ^.*ALLOW -oe ^.*DENY | sed -e s/DENY//g -e s/ALLOW//g)
+		proto=$(echo $line | grep -oe tcp -oe udp)
+		if [[ $proto == '' ]]
+		then
+			proto="ANY"
+		fi
+		action=$(echo $line | grep -oe ALLOW -oe DENY)
+		source=$(echo $line | awk ' { print $NF} ')
+		port_and_dest="Pi --> $port_and_dest (Out)"
 
                 # Pass fields to table function for HTML formatting
-		table $port $proto $action $source
+                table "$port_and_dest" "$proto" "$action" "$source"
+	else
+		# Determine if rule is by port or service and parse accordingly
+		if [[ $line != *"tcp"* && $line != *"udp"* ]]
+		then
+			port=$(echo $line | awk ' { print $1 } ')
+			port="Pi <-- $port (In)"
+	                action=$(echo $line | awk ' { print $2 } ')
+	                proto='N/A'
+	                source=$(echo $line | awk ' { print $3 } ')
+
+			# Pass fields to table function for HTML formatting
+  	              table "$port" "$proto" "$action" "$source"
+		else
+			port=$(echo $line | awk ' { print $1 } '| awk -F '/' ' { print $1 } ')
+                        port="Pi <-- $port (In)"
+	        	proto=$(echo $line | awk ' { print $1 } '| awk -F '/' ' { print $2 } ')
+			action=$(echo $line | awk ' { print $2 } ')
+	        	source=$(echo $line | awk ' { print $3 } ')
+
+	                # Pass fields to table function for HTML formatting
+			table "$port" "$proto" "$action" "$source"
+		fi
 	fi
 done
 
