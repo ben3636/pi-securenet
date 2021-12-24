7987b5bb52e7f441db265ebff78f8a04004be409
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2021-12-24 16:14:20 -0500

    Create alias.php

---
+++ scripts/alias.php
@@ -0,0 +1,5 @@
+<form action="submit.php" method="post">
+ <p>MAC Address: <input type="text" name="mac" /></p>
+ <p>Alias: <input type="text" name="alias" /></p>
+ <p><input type="submit" /></p>
+</form>
