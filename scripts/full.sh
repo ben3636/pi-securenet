0416eb7d5b09910b451181e19327867db5e136f5
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-01-03 13:26:29 -0500

    Added MAC/Hostname Enrichment & Reduced Redundant Scans

--- scripts/full.sh
+++ scripts/full.sh
@@ -39,13 +39,66 @@ do
         hostname=$(echo $line | awk ' { print $3 } ')
 	results=$(echo $line | grep -o Ports.*|sed -e s/"\/\/\/"/'<br>'/g -e s/"Ports: "//g -e s/","//g -e s/"Ignored State:.*$"//g -e s/'\/'/' '/g \
 	-e s/" open"/" - open"/g -e s/" filtered"/" - filtered"/g -e s/" closed"/" - closed"/g)
-	if [[ $hostname == "()" ]] || [[ $hostname == '' ]]
+	mac=''
+	if [[ $hostname == "()" ]]
 	then
-		hostname='N/A'
+		hostname=''
 	fi
 
+        # Perform MAC Vendor Lookup
+        if [[ $mac == '' ]]
+        then
+                #echo "Performing MAC Vendor Lookup...($ip)"
+                mac=$(nmap -sn -PR -T5 $ip | grep "^MAC" | sed s/"MAC Address: "//g)
+        fi
+
+        # Check for Custom Aliases for Device
+        mac_address=$(echo $mac | awk ' { print $1 } ')
+        if [[ $mac_address != '' ]]
+        then
+                alias=$(cat /var/www/html/alias.txt | grep "$mac_address" | awk -F " --> " ' { print $2 } ')
+                if [[ $alias != '' ]]
+                then
+                        #echo "Alias for $mac_address found!: $alias"
+                        if [[ $alias != '' ]] && [[ $mac != '' ]]
+                        then
+                                hostname="$alias -- $mac"
+                        elif [[ $alias != '' ]] && [[ $mac == '' ]]
+                        then
+                                hostname="$alias"
+                        fi
+                fi
+        fi
+
+        # Perform Hostname Lookup If Needed
+        if [[ $hostname == '' ]]
+        then
+                # Attempt to determine multicast/avahi/mdns hostname if no DNS name
+                #echo "Performing Hostname Discovery...($ip)"
+                hostname=$(avahi-browse -ratp | grep $ip | awk -F ";" ' { print $7 } ' | uniq)
+                if [[ $hostname != '' ]] && [[ $mac != '' ]]
+                then
+			hostname="$hostname -- $mac"
+                elif [[ $hostname != '' ]] && [[ $mac == '' ]]
+                then
+                	hostname="$hostname"
+		elif [[ $hostname == '' ]]
+                then
+                        hostname='N/A'
+                fi
+        fi
+
+	# Combine MAC and Hostname Depending on Enrichment Results
+        if [[ $hostname == 'N/A' ]] && [[ $mac != '' ]]
+        then
+                hostname="$mac"
+        elif [[ $hostname != "N/A" ]] && [[ $mac != '' ]] && [[ $alias == '' ]]
+        then
+                hostname="$hostname -- $mac"
+        fi
+
 	# Pass fields to table function for HTML formatting
-	table $ip $hostname "$results"
+	table $ip "$hostname" "$results"
 done
 
 ### Print second half of HTML after the table
