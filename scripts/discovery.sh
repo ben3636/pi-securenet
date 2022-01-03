176e78a955a3c1ce2aabe486aa6630595c6f52c8
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-01-03 12:11:05 -0500

    Reduced Overhead/Redundancy for Scan Efficiency

--- scripts/discovery.sh
+++ scripts/discovery.sh
@@ -24,7 +24,7 @@ mkdir -p scans
 date=$(date)
 
 ### Run Scan & Export to Report File
-nmap -sn -PR $subnet -T5 -oG /var/www/html/scans/"scan-$date" >> /dev/null
+nmap -sn -PR $subnet -T5 | awk '/Nmap scan report for/{printf $5;printf " "$6;}/MAC Address:/{print " => "$3, $4, $5, $6;}' > /var/www/html/scans/"scan-$date"
 
 ### Print first half of the HTML before the table
 cat Discovery-Scan.html.p1
@@ -33,30 +33,50 @@ cat Discovery-Scan.html.p1
 cat scans/"scan-$date" | grep -v ^# \
 | while read line
 do
-	ip=$(echo $line | awk ' { print $2 } ')
-        hostname=$(echo $line | awk ' { print $3 } ')
-	if [[ $hostname == "()" ]] || [[ $hostname == '' ]]
-	then
-		# Attempt to determine multicast/avahi/mdns hostname lookup if no DNS name
-		hostname=$(avahi-browse -ratp | grep $ip | awk -F ";" ' { print $7 } ' | uniq)
-		if [[ $hostname == "()" ]] || [[ $hostname == '' ]]
-		then
-			hostname='N/A'
-		fi
-	fi
+        mac=$(echo $line | awk -F ' => ' ' { print $2 } ')
 
-	# Perform MAC Lookup
-	mac=$(nmap -sn -PR -T5 $ip | grep "^MAC" | sed s/"MAC Address: "//g)
-	if [[ $hostname == 'N/A' ]]
-	then
-		hostname="$mac"
-	elif [[ $mac != '' ]]
-	then
-		hostname="$hostname -- $mac"
-	fi
+	# Determine if initial scan found hostname and parse accordingly
+        if [[ $(echo $line | awk -F ' => ' ' { print $1 } ' | wc -l) != 1 ]]
+        then
+                host=$(echo $line | awk -F ' => ' ' { print $1 } ' | awk ' { print $2 } ' | sed -e "s/(//g" -e "s/)//g")
+                hostname=$(echo $line | awk -F ' => ' ' { print $1 } ' | awk ' { print $1 } ')
+        elif [[ $(echo $line | awk -F ' => ' ' { print $1 } ' | wc -l) == 1 ]]
+        then
+                hostname=''
+                host=$(echo $line | awk -F ' => ' ' { print $1 } ' | awk ' { print $1 } ' | sed -e "s/(//g" -e "s/)//g")
+        fi
 
-	# Check for custom aliases for device
+        # Perform Hostname Lookup If Needed
+        if [[ $hostname == '' ]]
+        then
+                # Attempt to determine multicast/avahi/mdns hostname if no DNS name
+                #echo "Performing Hostname Discovery...($host)"
+                hostname=$(avahi-browse -ratp | grep $host | awk -F ";" ' { print $7 } ' | uniq)
+                if [[ $hostname == "()" ]] || [[ $hostname == '' ]]
+                then
+                        hostname='N/A'
+                fi
+        fi
+
+        # Perform MAC Vendor Lookup If Needed
+        if [[ $mac == '' ]]
+        then
+                #echo "Performing MAC Vendor Lookup...($host)"
+                mac=$(nmap -sn -PR -T5 $host | grep "^MAC" | sed s/"MAC Address: "//g)
+        fi
+
+	# Combine MAC and Hostname Depending on Enrichment Results
+        if [[ $hostname == 'N/A' ]] && [[ $mac != '' ]]
+        then
+                hostname="$mac"
+        elif [[ $hostname != "N/A" ]] && [[ $mac != '' ]]
+        then
+                hostname="$hostname -- $mac"
+        fi
+
+	# Check for Custom Aliases for Device
 	mac_address=$(echo $mac | awk ' { print $1 } ')
+	ip=$host
 	if [[ $mac_address != '' ]]
 	then
 		alias=$(cat /var/www/html/alias.txt | grep "$mac_address" | awk -F " --> " ' { print $2 } ')
