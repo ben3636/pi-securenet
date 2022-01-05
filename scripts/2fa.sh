a7c7af886b7a237b388de51bb4c1e18473775776
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-01-04 19:28:22 -0500

    Delete 2fa.sh

--- scripts/2fa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "New Key Created!"
-key=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 5)
-message="Pi IDS 2FA Key Is: "
-curl -X POST -H "Content-Type: application/json" -d '{"value1":"'"$message"'","value2":"'"$key"'","value3":"'""'"}' https://maker.ifttt.com/trigger/XXXXXXXXX >> /dev/null
-htpasswd -b /home/ben/.htpasswd ben $key
