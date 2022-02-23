ccb024149a58132aeb7ae45fbe89698beee2b52b
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-02-22 20:57:56 -0500

    Delete test-form.php

--- scripts/test-form.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type: text/html");
-$tgt=$_POST['name'];
-$cmd='sudo /var/www/html/t-scan.sh ';
-$comb=$cmd.$tgt;
-passthru($comb);
-// header('Location: http://www.google.com');
-?>
