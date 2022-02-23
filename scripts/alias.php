a3eedbd74d2717b5b6d62c87f5faed7bfc7ddb07
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-02-22 20:32:53 -0500

    Delete alias.php

--- scripts/alias.php
+++
@@ -1,5 +0,0 @@
-<form action="submit.php" method="post">
- <p>MAC Address: <input type="text" name="mac" /></p>
- <p>Alias: <input type="text" name="alias" /></p>
- <p><input type="submit" /></p>
-</form>
