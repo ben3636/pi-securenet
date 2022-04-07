28f003fafd551e0b55a2e2f7a40c94fa9e350bcb
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2022-04-07 12:00:07 -0400

    Update ip-update.sh

--- scripts/ip-update.sh
+++ scripts/ip-update.sh
@@ -4,6 +4,7 @@ rm /var/www/html/*.p2
 echo "-- $ip --"
 sleep 5
 sed -i -e s/"192.168.10.4"/"$ip"/g /var/www/html/*.html
+sed -i -e s/"192.168.10.4"/"$ip"/g /var/www/html/*.php
 ls -1 /var/www/html/ | grep .html | while read line
 do
         ~/pi-securenet/update.sh /var/www/html/"$line"
